36a26c318d673fe05ed266a6846ef811ccc49676
phatdeptraiii <vitphat350@gmail.com>
2024-03-04 20:35:36 +0700

    bai16

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,16 +20,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Blade::directive('datatime', function ($expression) {
-            $expression = trim($expression, '\'');
-            $expression = trim($expression, '"');
-            $dateObject = date_create($expression);
+        // Blade::directive('datatime', function ($expression) {
+        //     $expression = trim($expression, '\'');
+        //     $expression = trim($expression, '"');
+        //     $dateObject = date_create($expression);
 
-            if (!empty($dateObject)) {
-                $dateFormat = $dateObject->format('d/m/Y H:i:s');
-                return $dateFormat;
+        //     if (!empty($dateObject)) {
+        //         $dateFormat = $dateObject->format('d/m/Y H:i:s');
+        //         return $dateFormat;
+        //     }
+        //     return false;
+        // });
+        Blade::if('env', function ($value) {
+            //Trả vê giá trị local
+            if (config('app.env') === $value) {
+                return true;
             }
             return false;
         });
     }
+
 }
--- resources/views/layouts/clients/home.blade.php
+++ resources/views/layouts/clients/home.blade.php
@@ -87,6 +87,13 @@
             <h1>Trang chủ</h1>
             @datatime('2022-12-03 15:00:00')
             @datatime("2022-12-06 15:00:00")
+            @env(production')
+            <p>Môi trường production </p>
+            @elseenv('test')
+            <p>Không phải mt test</p>
+            @else
+            <p>Không phải dev</p>
+            @endenv
             <button type="button" class="show">Show</button>
             @endsection
 
